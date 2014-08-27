884a05fd071c8eae84041ac583eb3ade9181bd65
dmertl <dmertl@gmail.com>
2014-08-26 18:31:55 -0700

    Adding setuptools setup.py file to list dependency packages

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='StoutBeerNotifier',
+      version='1.0',
+      # packages=['licensingserver'],
+      install_requires=[
+          'lxml',
+          'unidecode'
+      ],
+)
